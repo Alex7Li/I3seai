bf54cd2d02b8572a0e1deac91641278b85aa6007
Alex Li <7alex7li@gmail.com>
2022-03-14 22:48:35 -0400

    Working rating logger

--- aporia_ratinglogger.py
+++ aporia_ratinglogger.py
@@ -3,7 +3,7 @@ from eat_kafka import get_data
 import model
 from datetime import datetime
 from consumer_factory import create_consumer
-
+# Evaluate performance of the model
 aporia.init(token="bcfdab4e222d1de5b441c793b0e415bc92a7e78c2bdf82b9dd104e03e2868f74", 
             environment="local-dev", 
             verbose=True)
@@ -11,12 +11,12 @@ def aporia_setup_ratinglogger():
     """
     Setup a schema in aporia to view the model.
     """
-    apr_model_version = "sandbox-version2"
+    apr_model_version = "sandbox-version3"
     apr_model_type = "ranking"
     apr_features_schema = {
         "created_at": "datetime",
         "user_id": "numeric",
-        "movie_name": "categorical",
+        "movie_name": "string",
     }
     apr_predictions_schema = {
         'rating': 'numeric'
@@ -39,7 +39,6 @@ for message in create_consumer():
         # <time>,<userid>,GET /rate/<movieid>=<rating>
         movie_id, rating_raw = data[-1][10:].split('=')
         true_rating = int(rating_raw)
-        print(f'read rating {message.value}')
         apr_prediction_id = data[0] + data[1]
         time = datetime.fromisoformat(data[0])
         user_id = int(data[1])
@@ -48,21 +47,26 @@ for message in create_consumer():
             "user_id": user_id,
             "movie_name": movie_id,
         }
-        predicted_rating = rec_model.get_similarity(
-            user_id, movie_id_to_model_id[movie_id]
-        ) * 4 + 1
+        if movie_id in movie_id_to_model_id:
+            predicted_rating = rec_model.get_similarity(
+                user_id, movie_id_to_model_id[movie_id]
+            ) * 4 + 1
+        else: # Movie is not in model training set
+            predicted_rating = None
+
         apr_prediction_dict = {
             "rating": predicted_rating,
         }
         apr_actual_dict = {
             "rating": true_rating,
         }
+        print(f'read rating {message.value}, {predicted_rating=} {true_rating=}')
 
         apr_model.log_prediction(
             id=apr_prediction_id,
             features=apr_features_dict,
             predictions=apr_prediction_dict,
-            actual=apr_actual_dict,
+            actuals=apr_actual_dict,
         )
 
         apr_model.flush()
